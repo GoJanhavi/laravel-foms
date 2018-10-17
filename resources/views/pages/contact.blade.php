aeb18c80e2841a5bbdc094756726f9a3bb6e607f
janhavi <jg688@njit.edu>
2018-10-17 10:16:03 -0400

    Feature: Modified pages to create a thank you message

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,15 +7,18 @@
             <p class="lead">Use this form to contact website owner.</p>
         </div>
         <div class="card-body">
-            <form action="/contact" method="post">
+            <form role="form" id="contact-form" action="{{route('contact.save')}}" method="post">
 
                 @csrf
 
                 <div class="form-group">
+                    <label for="name">Name</label>
+                    <input name="name" type="text" class="form-control" id="name" placeholder="First Name">
+                </div>
+                <div class="form-group">
                     <label for="email">Email address</label>
                     <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
                 </div>
-
                 <div class="form-group">
                     <label for="message">Message</label>
                     <textarea name="message" class="form-control" id="message" rows="3"></textarea>
---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container">
+        <h1 class="mt-3">Thank you {{$name}}</h1>
+        <p>Our team will respond to your message as soon as possible.</p>
+    </div>
+@endsection
